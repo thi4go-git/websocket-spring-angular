04267deed64969942845ce5bc3ab68b7c21595f0
thi4go-git <thi4go19@gmail.com>
2024-02-08 21:13:11 -0300

    asd

---
+++ websocket-back/src/test/java/com/dynns/cloudtecnologia/websocket/WebsocketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
